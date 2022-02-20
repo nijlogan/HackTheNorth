97e1bce7767432235bec2cb7e1f85382cb46f8e6
Jordan <jakj3m@gmail.com>
2022-02-20 01:32:16 -0500

    Update MainMenu.cs
    I've seen gridlock-inducing traffic jams that weren't as clustered up as this code.
    Also fixed an issue that would have broken the game like you broke my trust when you said you would show up today :(

--- Source/Assets/Scripts/Menu Scripts/MainMenu.cs
+++ Source/Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -30,47 +30,71 @@ public class MainMenu : MonoBehaviour
         dropdownMenu.ClearOptions();
         resolutions = new Resolution[8];
 
+        //Add 640 by 480 resolution option
         string option = "640 x 480";
+        resOps.Add(option);
+        
         Resolution res = new Resolution();
         res.width = 640;
         res.height = 480;
         resolutions[0] = res;
-        resOps.Add(option);
+       
+        //Add 800 by 600 resolution option
         option = "800 x 600";
+        resOps.Add(option);
+        
         res.width = 800;
         res.height = 600;
         resolutions[1] = res;
-        resOps.Add(option);
+        
+        //Add 960 by 720 resolution option
         option = "960 x 720";
+        resOps.Add(option);
+        
         res.width = 960;
         res.height = 720;
         resolutions[2] = res;
-        resOps.Add(option);
+        
+        //Add 1280 by 960 resolution option
         option = "1280 x 960";
+        resOps.Add(option);
+        
         res.width = 1280;
         res.height = 960;
         resolutions[3] = res;
-        resOps.Add(option);
+        
+        //Add 1440 by 1080 resolution option
         option = "1440 x 1080";
+        resOps.Add(option);
+        
         res.width = 1440;
         res.height = 1080;
         resolutions[4] = res;
-        resOps.Add(option);
+        
+        //Add 1600 by 1200 resolution option
         option = "1600 x 1200";
+        resOps.Add(option);
+        
         res.width = 1600;
         res.height = 1200;
         resolutions[5] = res;
-        resOps.Add(option);
+        
+        //Add 1920 by 1440 resolution option
         option = "1920 x 1440";
+        resOps.Add(option);
+        
         res.width = 1920;
         res.height = 1440;
         resolutions[6] = res;
-        resOps.Add(option);
+        
+        //Add 2880 by 2160 resolution option
         option = "2880 x 2160";
-        res.width = 12880;
+        resOps.Add(option);
+        
+        res.width = 2880;
         res.height = 2160;
         resolutions[7] = res;
-        resOps.Add(option);
+        
 
 
         int curRes = 0;
